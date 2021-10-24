8c42320f9f893171a2a44ac8fcd71407fc4a4047
Fiona Rose Fischer <fiona.fischer@student.htw-berlin.de>
2021-10-24 15:39:59 +0200

    first commit

---
+++ src/main/java/WebTech/webtech/web/HelloWorld.java
@@ -0,0 +1,13 @@
+package WebTech.webtech.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld{
+    @GetMapping(path = "/")
+    public ModelAndView showHelloWorldPage () {
+        return new ModelAndView("HelloWorld");
+    }
+}
---
+++ src/test/java/WebTech/webtech/WebTApplicationTests.java
@@ -0,0 +1,13 @@
+package WebTech.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
